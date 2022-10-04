f9be28609e39d42175785790436bfb2f80df1fb1
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-10-04 15:20:48 +0800

    Set up prisma

---
+++ data/users.ts
@@ -0,0 +1,19 @@
+export const users = [
+  {
+    username: "bryan",
+    email: "bryan@gmail.com",
+    password: "123456",
+    image: "some url",
+  },
+  {
+    username: "Jericho",
+    email: "jico.word@gmail.com",
+    password: "1234567",
+  },
+  {
+    username: "Ferd",
+    email: "ferd.working@gmail.com",
+    password: "1234356",
+    image: "some url",
+  },
+];
